9fa59e56a740d68c47e7b22d4e920c07ff886b69
Jula <133760567+juliapazdziorek@users.noreply.github.com>
2025-06-18 12:27:42 +0200

    feat: add exceptions handling, update type hints to use built-in list type for consistency

--- utils/files.py
+++ utils/files.py
@@ -1,24 +1,34 @@
-from typing import List
 
-
-def read_file(path: str) -> List[str]:
+def read_file(path: str) -> list[str]:
     """
     Reads a text file and returns a list of strings, where each string is a line from the file without a newline character.
     """
+    try:
+        with open(path, 'r') as file:
+            return [line.rstrip('\n') for line in file]
+        
+    except FileNotFoundError:
+        print(f"Error: File not found: {path}")
+        return []
+    except IOError as e:
+        print(f"Error reading file {path}: {e}")
+        return []
 
-    with open(path, 'r') as file:
-        return [line.rstrip('\n') for line in file]
 
-
-def read_file_into_2d_array(path: str) -> List[List[str]]:
+def read_file_into_2d_array(path: str) -> list[list[str]]:
     """
     Reads a text file and returns its contents as a 2D array (list of lists of characters),
     where each sublist represents a line of characters.
     """
     rows = []
-
-    with open(path, 'r') as file:
-        for line in file:
-            line = line.rstrip('\n')
-            rows.append(list(line))
+    try:
+        with open(path, 'r') as file:
+            for line in file:
+                line = line.rstrip('\n')
+                rows.append(list(line))
+                
+    except FileNotFoundError:
+        print(f"Error: File not found: {path}")
+    except IOError as e:
+        print(f"Error reading file {path}: {e}")
     return rows
